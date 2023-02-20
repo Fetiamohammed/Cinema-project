2ad3b02b555f0025c2df5e316a3181f4bfb74f29
Fetiamohammed <fetia-awol@hotmail.com>
2023-02-21 00:09:32 +0100

    changes 1

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+//import bootstarap
+import "bootstrap/dist/css/bootstrap.css";
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
+import App from "./App.vue";
+import router from "./router";
 
-import App from './App.vue'
-import router from './router'
+import "./assets/base.css";
 
-import './assets/main.css'
+const app = createApp(App);
+app.use(bootstrap);
+app.use(createPinia());
+app.use(router);
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
